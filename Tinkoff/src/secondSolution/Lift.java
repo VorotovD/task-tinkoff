83c0834d95df46aa50f7f77eacfe7ac5492e050c
Воротов Даниил <swarog.5611@yandex.ru>
2022-08-17 21:46:19 +0300

    Refactoring

--- Tinkoff/src/secondSolution/Lift.java
+++ Tinkoff/src/secondSolution/Lift.java
@@ -18,17 +18,4 @@ public class Lift {
     public int getEndFloor() {
         return endFloor;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Lift lift = (Lift) o;
-        return startFloor == lift.startFloor && endFloor == lift.endFloor;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(startFloor, endFloor);
-    }
 }
