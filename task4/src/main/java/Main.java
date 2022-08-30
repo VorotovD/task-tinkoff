bd1228b29f8050665209ae189faff3e892884c7d
Воротов Даниил <swarog.5611@yandex.ru>
2022-08-30 21:55:55 +0300

    Рефакторинг

--- task4/src/main/java/Main.java
+++ task4/src/main/java/Main.java
@@ -1,31 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> subscriptions = new ArrayList<>();
-        subscriptions.add(3); // positive
-        subscriptions.add(4); // negative
-        subscriptions.add(10); // positive
-        subscriptions.add(12); // negative
-        subscriptions.add(2); // positive
-        subscriptions.add(1); // negative
+        Integer[] subscriptions = new Integer[6];
+        subscriptions[0] = 3; // positive
+        subscriptions[1] = 4; // negative
+        subscriptions[2] = 10; // positive
+        subscriptions[3] = 12; // negative
+        subscriptions[4] = 2; // positive
+        subscriptions[5] = 1; // negative
         int minPositive = Integer.MAX_VALUE;
+        int minIndex = 0;
         int maxNegative = Integer.MIN_VALUE;
+        int maxIndex = 0;
         boolean positive = true;
-        for (int candidate : subscriptions) {
+        for (int i = 0; i < subscriptions.length; i++) {
+            int candidate = subscriptions[i];
             if (positive) {
                 positive = false;
                 if (candidate < minPositive) {
                     minPositive = candidate;
+                    minIndex = i;
                 }
             } else {
                 positive = true;
                 if (candidate > maxNegative) {
                     maxNegative = candidate;
+                    maxIndex = i;
                 }
             }
         }
-        System.out.println("Максимальная прибыль: "+(maxNegative-minPositive));
+        subscriptions[minIndex] = maxNegative;
+        subscriptions[maxIndex] = minPositive;
+        System.out.println("Максимальная прибыль: " + getProfit(subscriptions));
+    }
+
+    public static int getProfit(Integer[] subscriptions) {
+        int profit = 0;
+        for (int i = 0; i < subscriptions.length; i++) {
+            if (i % 2 == 0) {
+                profit += subscriptions[i];
+            } else {
+                profit -= subscriptions[i];
+            }
+        }
+        return profit;
     }
 }
