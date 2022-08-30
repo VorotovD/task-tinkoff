78a3915d9797c900c73d02b039467aec21133df3
Воротов Алексей Викторович <Vorotov.A.Vikt@sberbank.ru>
2022-08-30 19:55:04 +0300

    Рефакторинга

--- task4/src/main/java/Main.java
+++ task4/src/main/java/Main.java
@@ -10,6 +10,12 @@ public class Main {
         subscriptions.add(12); // negative
         subscriptions.add(2); // positive
         subscriptions.add(1); // negative
+        System.out.println("Прибыль до улучшения: " + profit(subscriptions));
+        improveProfit(subscriptions);
+        System.out.println("Прибыль после улучшения: " + profit(subscriptions));
+    }
+
+    static void improveProfit(List<Integer> subscriptions) {
         int minPositive = Integer.MAX_VALUE;
         int maxNegative = Integer.MIN_VALUE;
         boolean positive = true;
@@ -26,6 +32,23 @@ public class Main {
                 }
             }
         }
-        System.out.println("Максимальная прибыль: "+(maxNegative-minPositive));
     }
+
+    static int profit(List<Integer> subscriptions) {
+        boolean positive = true;
+        int profit = 0;
+        for (Integer subscription : subscriptions) {
+            if (positive) {
+                profit += subscription;
+            } else {
+                profit -= subscription;
+            }
+            positive = !positive;
+        }
+        return profit;
+    }
+
 }
+
+
+
